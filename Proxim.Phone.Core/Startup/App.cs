5c3397cd0ee9f27a739b37e2a97063fa658d6c92
flovo89 <flovo89@hotmail.com>
2017-02-16 20:43:58 +0100

    add portable project

---
+++ Proxim.Phone.Core/Startup/App.cs
@@ -0,0 +1,27 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.IoC;
+
+
+
+
+namespace Proxim.Phone.Core.Startup
+{
+    public class App : MvxApplication
+    {
+        #region Overrides
+
+        public override void Initialize ()
+        {
+            this.CreatableTypes().EndingWith("Service").AsInterfaces().RegisterAsLazySingleton();
+
+            Mvx.ConstructAndRegisterSingleton<IMvxAppStart, AppStart>();
+
+            IMvxAppStart appStart = Mvx.Resolve<IMvxAppStart>();
+
+            this.RegisterAppStart(appStart);
+        }
+
+        #endregion
+    }
+}
---
+++ Proxim.Phone.Core/Startup/AppStart.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.Core.ViewModels;
+
+
+
+
+namespace Proxim.Phone.Core.Startup
+{
+    public class AppStart : MvxNavigatingObject,
+            IMvxAppStart
+    {
+        #region Interface: IMvxAppStart
+
+        public void Start (object hint = null)
+        {
+            //Mvx.Resolve<ISessionService>().Initialize();
+            //Mvx.Resolve<IDialogService>().Initialize();
+            //Mvx.Resolve<IResourceService>().Initialize();
+            //Mvx.Resolve<ILocationService>().Initialize();
+            //Mvx.Resolve<ILoggingService>().Initialize();
+            //Mvx.Resolve<IMessagingService>().Initialize();
+            //Mvx.Resolve<ICommunicationService>().Initialize();
+            //Mvx.Resolve<ILoginService>().Initialize();
+
+            //this.ShowViewModel<MainViewModel>();
+        }
+
+        #endregion
+    }
+}
