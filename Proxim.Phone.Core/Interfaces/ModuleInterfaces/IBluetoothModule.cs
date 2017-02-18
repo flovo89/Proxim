aa173954529440a149ed52f925df5a754eb290d9
flovo89 <flovo89@hotmail.com>
2017-02-18 18:50:24 +0100

    blauzahn

--- Proxim.Phone.Core/Interfaces/ModuleInterfaces/IBluetoothModule.cs
+++ Proxim.Phone.Core/Interfaces/ModuleInterfaces/IBluetoothModule.cs
@@ -1,6 +1,20 @@
-﻿namespace Proxim.Phone.Core.Interfaces.ModuleInterfaces
+﻿using Plugin.BLE.Abstractions.Contracts;
+
+
+
+
+namespace Proxim.Phone.Core.Interfaces.ModuleInterfaces
 {
     public interface IBluetoothModule : IModule
     {
+        bool GetBluetoothAvailable ();
+
+        bool GetBluetoothEnabled ();
+
+        BluetoothState GetBluetoothState ();
+
+        void StartAdvertising ();
+
+        void StopAdvertising ();
     }
 }
--- Proxim.Phone.Droid/Modules/Bluetooth/BluetoothModule.cs
+++ Proxim.Phone.Droid/Modules/Bluetooth/BluetoothModule.cs
@@ -1,3 +1,9 @@
+﻿using Android.Bluetooth.LE;
+
+using MvvmCross.Platform;
+
+using Plugin.BLE.Abstractions.Contracts;
+
 using Proxim.Phone.Core.Interfaces.ModuleInterfaces;
 
 
@@ -7,9 +13,52 @@ namespace Proxim.Phone.Droid.Modules.Bluetooth
 {
     public class BluetoothModule : IBluetoothModule
     {
-        public void Initialize ()
+        #region Instance Properties/Indexer
+
+        private IAdapter Adapter { get; set; }
+
+        private IBluetoothLE BluetoothLe { get; set; }
+
+        private BluetoothLeAdvertiser BluetoothLeAdvertiser { get; set; }
+
+        #endregion
+
+
+        //TODO: Check how to use BluetoothLeAdvertiser
+
+        #region Interface: IBluetoothModule
+
+        public bool GetBluetoothAvailable ()
+        {
+            return this.BluetoothLe.IsAvailable;
+        }
+
+        public bool GetBluetoothEnabled ()
+        {
+            return this.BluetoothLe.IsOn;
+        }
+
+        public BluetoothState GetBluetoothState ()
+        {
+            return this.BluetoothLe.State;
+        }
+
+        public void StartAdvertising ()
         {
             throw new System.NotImplementedException();
         }
+
+        public void StopAdvertising ()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Initialize ()
+        {
+            this.BluetoothLe = Mvx.Resolve<IBluetoothLE>();
+            this.Adapter = Mvx.Resolve<IAdapter>();
+        }
+
+        #endregion
     }
 }
