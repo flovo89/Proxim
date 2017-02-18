b30cdf87ba6bad1eec5c498f6beb10a2c3458cde
flovo <flovo@LAPTOP-FTQ6K5QH>
2017-02-18 14:30:17 +0100

    dialogservice

---
+++ Proxim.Phone.Droid/Services/Dialog/DialogService.cs
@@ -0,0 +1,37 @@
+using Android.App;
+
+using MvvmCross.Platform;
+using MvvmCross.Platform.Droid.Platform;
+
+using Proxim.Phone.Core.Interfaces;
+
+
+
+
+namespace Proxim.Phone.Droid.Services.Dialog
+{
+    public class DialogService : IDialogService
+    {
+        #region Interface: IDialogService
+
+        public void Alert (string message, string btnText)
+        {
+            IMvxAndroidCurrentTopActivity top = Mvx.Resolve<IMvxAndroidCurrentTopActivity>();
+            Activity act = top.Activity;
+
+            AlertDialog.Builder adb = new AlertDialog.Builder(act);
+            adb.SetMessage(message);
+            adb.SetPositiveButton(btnText, (sender, args) =>
+            {
+                //No logic necessary!
+            });
+            adb.Create().Show();
+        }
+
+        public void Initialize ()
+        {
+        }
+
+        #endregion
+    }
+}
